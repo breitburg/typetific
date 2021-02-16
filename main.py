d010a5690e0fa37f483c5e6c1a34d66109cffc66
breitburg <me@breitburg.com>
2021-02-16 07:28:47 +0300

    Settings keys now in camel case

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class TypingWindow(QWidget):
         self.config = {
             'error_color': QColor(255, 190, 190),
             'done_color': QColor(190, 255, 190),
-            'secondary_color': QColor(215, 215, 215),
+            'secondary_color': QColor(230, 230, 230),
             'background_color': QColor(255, 255, 255),
             'primary_color': QColor(34, 34, 34),
             'dictionary': english_words_set
@@ -32,7 +32,7 @@ class TypingWindow(QWidget):
         self.painter.begin(self)
 
         # Limiting the font size to 32
-        self.painter.setFont(QFont('Menlo', max(32, event.rect().height() / self.settings.value('font_factor', 6))))
+        self.painter.setFont(QFont('Menlo', max(32, event.rect().height() / self.settings.value('fontFactor', 6))))
         self.isEnd = self.pointer >= len(self.text)
 
         # Calculating line properties
@@ -41,9 +41,9 @@ class TypingWindow(QWidget):
         lineColor = self.config['done_color'] if self.isEnd else (
             self.config['error_color'] if len(self.events) > 0 and not self.events[-1][
                 'correct'] else self.config['secondary_color'])
-        lineWidth = letterSize.maxWidth() + self.settings.value('line_padding', 10)
+        lineWidth = letterSize.maxWidth() + self.settings.value('linePadding', 10)
         linePadding = event.rect().width() / 4
-        lineX = linePadding if not self.settings.value('enable_sliding', True) else linePadding + (
+        lineX = linePadding if not self.settings.value('enableSliding', False) else linePadding + (
                 ((event.rect().width() - lineWidth) - (linePadding * 2)) / len(self.text)) * self.pointer
 
         # Rendering line
@@ -54,8 +54,8 @@ class TypingWindow(QWidget):
         # Primary Text
         if not self.isEnd:
             self.painter.setPen(self.config['primary_color'])
-            self.painter.drawText(lineX + self.settings.value('line_padding', 10) / 2, textY, self.text[self.pointer])
-            self.painter.drawText(lineX + lineWidth + self.settings.value('line_padding', 10) / 2, textY,
+            self.painter.drawText(lineX + self.settings.value('linePadding', 10) / 2, textY, self.text[self.pointer])
+            self.painter.drawText(lineX + lineWidth + self.settings.value('linePadding', 10) / 2, textY,
                                   self.text[self.pointer + 1:])
 
         if len(self.events) > 1:
@@ -70,7 +70,7 @@ class TypingWindow(QWidget):
         # Typed Text
         typedTextSize = self.painter.fontMetrics().size(Qt.TextSingleLine, self.text[:self.pointer])
         self.painter.setPen(self.config['secondary_color'])
-        self.painter.drawText(lineX - typedTextSize.width() - self.settings.value('line_padding', 10) / 2, textY,
+        self.painter.drawText(lineX - typedTextSize.width() - self.settings.value('linePadding', 10) / 2, textY,
                               self.text[:self.pointer])
 
         self.painter.end()
@@ -78,7 +78,7 @@ class TypingWindow(QWidget):
     def keyPressEvent(self, event) -> None:
         if event.key() == Qt.Key_Return:
             self.reset()
-        elif self.settings.value('enable_backspace', True) and event.key() == Qt.Key_Backspace and self.pointer > 0:
+        elif self.settings.value('enableBackspace', True) and event.key() == Qt.Key_Backspace and self.pointer > 0:
             self.pointer -= 1
         elif event.key() not in [ord(char) for char in printable] or self.isEnd:
             return
@@ -98,7 +98,7 @@ class TypingWindow(QWidget):
     def reset(self) -> None:
         self.events = []
         self.text = ' '.join(
-            [choice(list(self.config['dictionary'])).lower() for _ in range(self.settings.value('words_count', 20))])
+            [choice(list(self.config['dictionary'])).lower() for _ in range(self.settings.value('wordsCount', 20))])
         self.pointer = 0
 
     def setupWindow(self) -> None:
